90f0c240c7e5b1f57b4d0164544d9d84ea406a0e
Deniel <huangdb@worldin.cn>
2016-10-11 19:33:28 +0800

    update ios alipay

--- plugins/com.yoopoon.cordova.plugin.alipay/src/ios/CDVAlipay.m
+++ plugins/com.yoopoon.cordova.plugin.alipay/src/ios/CDVAlipay.m
@@ -77,53 +77,54 @@
 }
 -(void)pay1:(CDVInvokedUrlCommand *)command{
     NSDictionary *orderInfoArgs = [self checkArgs:command];
+    NSString *orderString = orderInfoArgs[@"payInfo"];
     
-    NSString *subject = orderInfoArgs[@"subject"];
-    NSString *body = orderInfoArgs[@"body"];
-    NSString *price = orderInfoArgs[@"price"];
-    NSString *tradeNo = orderInfoArgs[@"tradeNo"];
-    NSString *timeout = orderInfoArgs[@"timeout"];
-    NSString *notifyUrl = orderInfoArgs[@"notifyUrl"];
-    NSString *seller = orderInfoArgs[@"seller"];
-    self.partner = orderInfoArgs[@"partner"];
-    self.rsa_private = orderInfoArgs[@"rsa_private"];
-    self.rsa_public = orderInfoArgs[@"rsa_public"];
+    // NSString *subject = orderInfoArgs[@"subject"];
+    // NSString *body = orderInfoArgs[@"body"];
+    // NSString *price = orderInfoArgs[@"price"];
+    // NSString *tradeNo = orderInfoArgs[@"tradeNo"];
+    // NSString *timeout = orderInfoArgs[@"timeout"];
+    // NSString *notifyUrl = orderInfoArgs[@"notifyUrl"];
+    // NSString *seller = orderInfoArgs[@"seller"];
+    // self.partner = orderInfoArgs[@"partner"];
+    // self.rsa_private = orderInfoArgs[@"rsa_private"];
+    // self.rsa_public = orderInfoArgs[@"rsa_public"];
     //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
     NSString *appScheme = @"alipaycordova";
     /*
      *生成订单信息及签名
      */
     //将商品信息赋予AlixPayOrder的成员变量
-    Order *order = [[Order alloc] init];
-    order.partner = self.partner;
-    order.sellerID = seller;
-    order.outTradeNO = tradeNo; //订单ID（由商家自行制定）
-    order.subject = subject; //商品标题
-    order.body = body; //商品描述
-    order.totalFee = price; //商品价格
-    order.notifyURL =  notifyUrl; //回调URL
+    // Order *order = [[Order alloc] init];
+    // order.partner = self.partner;
+    // order.sellerID = seller;
+    // order.outTradeNO = tradeNo; //订单ID（由商家自行制定）
+    // order.subject = subject; //商品标题
+    // order.body = body; //商品描述
+    // order.totalFee = price; //商品价格
+    // order.notifyURL =  notifyUrl; //回调URL
     
-    order.service = @"mobile.securitypay.pay";
-    order.paymentType = @"1";
-    order.inputCharset = @"utf-8";
-    order.itBPay = timeout;
-    order.showURL = @"m.alipay.com";
+    // order.service = @"mobile.securitypay.pay";
+    // order.paymentType = @"1";
+    // order.inputCharset = @"utf-8";
+    // order.itBPay = timeout;
+    // order.showURL = @"m.alipay.com";
     
     //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
     
     //将商品信息拼接成字符串
-    NSString *orderSpec = [order description];
-    NSLog(@"orderSpec = %@",orderSpec);
+    // NSString *orderSpec = [order description];
+    // NSLog(@"orderSpec = %@",orderSpec);
     
-    //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
-    id<DataSigner> signer = CreateRSADataSigner(self.rsa_private);
-    NSString *signedString = [signer signString:orderSpec];
+    // //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
+    // id<DataSigner> signer = CreateRSADataSigner(self.rsa_private);
+    // NSString *signedString = [signer signString:orderSpec];
     
-    //将签名成功字符串格式化为订单字符串,请严格按照该格式
-    NSString *orderString = nil;
-    if (signedString != nil) {
-        orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
-                       orderSpec, signedString, @"RSA"];
+    // //将签名成功字符串格式化为订单字符串,请严格按照该格式
+    // NSString *orderString = nil;
+    // if (signedString != nil) {
+    //     orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
+    //                    orderSpec, signedString, @"RSA"];
         
         [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
             NSLog(@"reslut = %@",resultDic);
@@ -134,6 +135,6 @@
         }];
     }
     
-}
+//}
 
 @end
